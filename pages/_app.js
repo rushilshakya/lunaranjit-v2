b88f4d6212d3fb827452cbeae3082da6dbac9437
rushilshakya <rushil.shakya@gmail.com>
2024-05-30 17:07:23 +0545

    fix: change order of styles loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import "@/styles/scss/style.scss";
 import "@/styles/plugins/bootstrap/bootstrap.min.css";
 import "@/styles/plugins/themify-icons/themify-icons.css";
+import "@/styles/scss/style.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
