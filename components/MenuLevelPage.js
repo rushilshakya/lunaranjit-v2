051b0eac0c0eef1e787361eabf841e193c6d1358
rushilshakya <rushil.shakya@gmail.com>
2024-06-01 21:57:20 +0545

    feat: add menu level page for write

---
+++ components/MenuLevelPage.js
@@ -0,0 +1,29 @@
+import Markdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+
+export default function MenuLevelPage({ pageData }) {
+  return (
+    <>
+      <section className="section-sm bg-aliceblue">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 mx-auto text-center">
+              <h1 className="text-center">{pageData.title}</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <div className="col-lg-8 mx-auto">
+            <div className="content">
+              <Markdown rehypePlugins={[rehypeRaw]}>
+                {pageData.content}
+              </Markdown>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
