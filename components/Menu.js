a63d753201b1515b6c6dd352c97baafa725116ce
rushilshakya <rushil.shakya@gmail.com>
2024-06-03 17:00:22 +0545

    refactor: to Image and Link

--- components/Menu.js
+++ components/Menu.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export const Menu = () => {
   const [showSearch, setShowSearch] = useState("");
@@ -31,13 +33,15 @@ export const Menu = () => {
     <header>
       <div className="container-fluid">
         <nav className="navbar navbar-expand-lg navbar-light">
-          <a className="navbar-brand" href="/">
-            <img
+          <Link className="navbar-brand" href="/">
+            <Image
               className="img-logo"
               src="/images/lr-logo.svg"
               alt="Luna Ranjit"
+              width={100}
+              height={100}
             />
-          </a>
+          </Link>
           <button
             className="navbar-toggler border-0"
             type="button"
@@ -49,37 +53,37 @@ export const Menu = () => {
           <div className="collapse navbar-collapse text-center" id="navigation">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" href="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">
+                <Link className="nav-link" href="/about">
                   About
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/write">
+                <Link className="nav-link" href="/write">
                   Writing
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/speak">
+                <Link className="nav-link" href="/speak">
                   Speaking
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/collaborate">
+                <Link className="nav-link" href="/collaborate">
                   Collaborating
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/contact">
+                <Link className="nav-link" href="/contact">
                   Contact
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="nav-link"
                   href="https://twitter.com/LunaRanjit"
                   target="_blank"
@@ -94,21 +98,26 @@ export const Menu = () => {
                   >
                     <path d="M5.026 15c6.038.0 9.341-5.003 9.341-9.334.0-.14.0-.282-.006-.422A6.685 6.685.0 0016 3.542a6.658 6.658.0 01-1.889.518 3.301 3.301.0 001.447-1.817 6.533 6.533.0 01-2.087.793A3.286 3.286.0 007.875 6.03 9.325 9.325.0 011.108 2.601a3.289 3.289.0 001.018 4.382A3.323 3.323.0 01.64 6.575v.045a3.288 3.288.0 002.632 3.218 3.203 3.203.0 01-.865.115 3.23 3.23.0 01-.614-.057 3.283 3.283.0 003.067 2.277A6.588 6.588.0 01.78 13.58 6.32 6.32.0 010 13.535 9.344 9.344.0 005.026 15z"></path>
                   </svg>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="nav-link"
                   href="https://lunaranjit.medium.com"
                   target="_blank"
                 >
-                  <img src="/images/medium.svg" alt="Medium logo" />
-                </a>
+                  <Image
+                    width={18}
+                    height={18}
+                    src="/images/medium.svg"
+                    alt="Medium logo"
+                  />
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/books">
+                <Link className="nav-link" href="/books">
                   Alien
-                </a>
+                </Link>
               </li>
             </ul>
             <div className="search">
