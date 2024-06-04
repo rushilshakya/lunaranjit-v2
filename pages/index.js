df41d5031f5be191f18c1dde11540ead4a031025
rushilshakya <rushil.shakya@gmail.com>
2024-06-04 21:36:09 +0545

    chore: add todo

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home({ posts, pinnedPost, totalPages }) {
 /*
 TODO: Tiny letter doesn't work anymore
 TODO: Links in content to be differentiated?
+TODO: configure tina
 
 DONE: 404 page
 DONE: External links to open in new tab
