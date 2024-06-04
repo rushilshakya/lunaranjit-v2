80036671f90266ad2825b083702a3b668d0612c2
rushilshakya <rushil.shakya@gmail.com>
2024-06-04 16:49:46 +0545

    chore: change posts folder to post

--- tina/config.js
+++ tina/config.js
@@ -31,7 +31,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "content/posts",
+        path: "content/post",
         fields: [
           {
             type: "string",
