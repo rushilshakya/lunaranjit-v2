10a418b993e5849ca53395170f0064de9bd79d0e
rushilshakya <rushil.shakya@gmail.com>
2024-06-09 15:02:26 +0545

    chore: build for SSG

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   distDir: "dist",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
