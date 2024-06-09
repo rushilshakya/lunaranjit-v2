0c7960476cc3f23e3b7beecd669b283f71b291f5
rushilshakya <rushil.shakya@gmail.com>
2024-06-09 15:17:39 +0545

    gh-pages first commit!

---
+++ _next/static/Ip-tro32cbMIcSolFBbD2/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fauthor\u002F[slug]","\u002Fbooks","\u002Fcontact","\u002Forganize","\u002Fpage\u002F[slug]","\u002Fpost\u002F[slug]","\u002Fsearch","\u002Fspeak","\u002Ftag\u002F[slug]","\u002Fwrite"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
