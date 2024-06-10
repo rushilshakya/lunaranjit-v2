2709c8fddc7c0db047037b6dcc93c846b1085882
rushilshakya <rushil.shakya@gmail.com>
2024-06-10 17:07:16 +0545

    fix: get google font to work with nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { Playfair_Display } from "next/font/google";
 import "@/styles/plugins/bootstrap/bootstrap.min.css";
 import "@/styles/plugins/themify-icons/themify-icons.css";
 import "@/styles/scss/style.scss";
 
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          --font-playfairDisplay: ${playfairDisplay.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
