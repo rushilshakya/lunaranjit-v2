c9186ee471c4bdf61bd1743d29547c34fbca5d71
rushilshakya <rushil.shakya@gmail.com>
2024-06-10 09:26:19 +0545

    refactor: add font through nextjs way

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,22 @@
+import { Playfair_Display } from "next/font/google";
 import { HeadMatter } from "./HeadMatter";
 import { Menu } from "./Menu";
 import { Footer } from "./Footer";
 
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-playfairDisplay",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
 export const Layout = ({ children }) => {
   return (
-    <>
+    <body className={`${playfairDisplay.variable}`}>
       <HeadMatter />
       <Menu />
       {children}
       <Footer />
-    </>
+    </body>
   );
 };
