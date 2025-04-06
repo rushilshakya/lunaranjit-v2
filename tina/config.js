288bb1afe7550cd06c4edaf677dcfb4db85c8460
rushilshakya <rushil.shakya@gmail.com>
2025-04-07 01:07:46 +0700

    chore: fix multiple slashes in image

--- tina/config.js
+++ tina/config.js
@@ -136,6 +136,11 @@ export default defineConfig({
             required: true,
             ui: {
               parse(value) {
+                //replace multiple slashes with a single slash
+                console.log("value", value);
+                value = value.replace(/\/\/+/, "/");
+                console.log("value", value);
+
                 //add leading slash to value if it doesnt exist
                 return value.startsWith("/") ? value : `/${value}`;
               },
